e2dd563afa2e2817c91e8e54e49eea939d847ccc
martin.wang <martin.wang@sonicsky.net>
2022-08-10 22:46:35 +0800

    Add + / - buttons

--- app/src/main/java/com/bigcake/bmicalculator/ui/main/MainViewModel.kt
+++ app/src/main/java/com/bigcake/bmicalculator/ui/main/MainViewModel.kt
@@ -7,7 +7,6 @@ import java.math.RoundingMode
 import kotlin.math.pow
 
 class MainViewModel : ViewModel() {
-    //    private val bmiFormat by lazy { DecimalFormat("#.##") }
     private var _weight = MutableStateFlow(50)
     val weight = _weight.asStateFlow()
     private var _height = MutableStateFlow(175)
@@ -15,13 +14,13 @@ class MainViewModel : ViewModel() {
     private var _bmi = MutableStateFlow(0f)
     val bmi = _bmi.asStateFlow()
 
-    fun onWeightChanged() {
-        _weight.value = weight.value.inc()
+    fun onWeightChangedBy(amount: Int) {
+        _weight.value += amount
         updateBmi()
     }
 
-    fun onHeightChanged() {
-        _height.value = height.value.inc()
+    fun onHeightChangedBy(amount: Int) {
+        _height.value += amount
         updateBmi()
     }
 
